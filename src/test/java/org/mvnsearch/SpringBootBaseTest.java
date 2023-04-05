5f522093bf12ae285446df9559af81eb38172a8b
Libing Chen <libing.chen@gmail.com>
2023-04-05 18:21:07 +0800

    test: SpringBootBaseTest

---
+++ src/test/java/org/mvnsearch/SpringBootBaseTest.java
@@ -0,0 +1,10 @@
+package org.mvnsearch;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class SpringBootBaseTest {
+
+}
