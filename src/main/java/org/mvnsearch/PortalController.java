5a147d7b4bd4e7450a19b490a735f75cbba6e5b1
Libing Chen <libing.chen@gmail.com>
2023-04-05 18:20:51 +0800

    app: PortalController

---
+++ src/main/java/org/mvnsearch/PortalController.java
@@ -0,0 +1,19 @@
+package org.mvnsearch;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+public class PortalController {
+    @Autowired
+    private HttpBinService httpBinService;
+
+    @GetMapping("/my-ip")
+    public Mono<String> myIp() {
+        return httpBinService.myIp().map(HttpBinService.MyIp::origin);
+    }
+
+}
