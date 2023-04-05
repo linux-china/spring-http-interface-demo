7f1007e8417a86a1da67dd04e306ce69733da6de
Libing Chen <libing.chen@gmail.com>
2023-04-05 18:22:33 +0800

    test: HttpBinClientTest

---
+++ src/test/java/org/mvnsearch/httpinterface/HttpBinClientTest.java
@@ -0,0 +1,24 @@
+package org.mvnsearch.httpinterface;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+public class HttpBinClientTest {
+    private static HttpBinClient httpBinClient;
+
+    @BeforeAll
+    public static void setUp() {
+        WebClient webClient = WebClient.builder().build();
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builder().clientAdapter(WebClientAdapter.forClient(webClient)).build();
+        httpBinClient = httpServiceProxyFactory.createClient(HttpBinClient.class);
+    }
+
+    @Test
+    public void testClient() throws Exception {
+        System.out.println(httpBinClient.myIp().block().origin());
+    }
+
+}
