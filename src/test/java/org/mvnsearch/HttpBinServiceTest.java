7ffb998cd22b8c1f9bdfd7aa632b42c547b6b12b
Libing Chen <libing.chen@gmail.com>
2023-04-05 18:22:59 +0800

    test: HttpBinServiceTest

---
+++ src/test/java/org/mvnsearch/HttpBinServiceTest.java
@@ -0,0 +1,18 @@
+package org.mvnsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.test.StepVerifier;
+
+
+public class HttpBinServiceTest extends SpringBootBaseTest {
+    @Autowired
+    private HttpBinService httpBinService;
+
+    @Test
+    public void testMyIp() {
+        StepVerifier.create(httpBinService.myIp())
+                .assertNext(myIp -> System.out.println(myIp.origin()))
+                .verifyComplete();
+    }
+}
