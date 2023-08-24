85691d2813906600142eeb92cad79f31e096c826
Libing Chen <libing.chen@gmail.com>
2023-08-24 23:35:36 +0800

    chore: moved

--- src/main/java/org/mvnsearch/web/rest/PortalController.java
+++ src/main/java/org/mvnsearch/web/rest/PortalController.java
@@ -1,5 +1,6 @@
-package org.mvnsearch;
+package org.mvnsearch.web.rest;
 
+import org.mvnsearch.service.HttpBinService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
