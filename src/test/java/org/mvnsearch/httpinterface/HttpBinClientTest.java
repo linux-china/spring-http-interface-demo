3deb7b5339f3a33a0835baa4e11b640d7174f54b
Libing Chen <libing.chen@gmail.com>
2023-10-05 13:11:41 +0800

    chore: add filter

--- src/test/java/org/mvnsearch/httpinterface/HttpBinClientTest.java
+++ src/test/java/org/mvnsearch/httpinterface/HttpBinClientTest.java
@@ -11,7 +11,7 @@ public class HttpBinClientTest {
 
     @BeforeAll
     public static void setUp() {
-        WebClient webClient = WebClient.builder().baseUrl("https://httpbin.org").build();
+        WebClient webClient = WebClient.builder().baseUrl("https://httpbin.org").filter(new LoggingFilter()).build();
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builder().exchangeAdapter(WebClientAdapter.forClient(webClient)).build();
         httpBinClient = httpServiceProxyFactory.createClient(HttpBinClient.class);
     }
