5fe140534821f27dde7fafacbe4e9a12f04a824c
Libing Chen <libing.chen@gmail.com>
2024-10-10 01:25:41 +0800

    chore: add upload

--- src/test/java/org/mvnsearch/httpinterface/HttpBinClient.java
+++ src/test/java/org/mvnsearch/httpinterface/HttpBinClient.java
@@ -1,8 +1,12 @@
 package org.mvnsearch.httpinterface;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.PostExchange;
 import reactor.core.publisher.Mono;
+
 
 public interface HttpBinClient {
     @GetExchange("/ip")
@@ -10,6 +14,9 @@ public interface HttpBinClient {
 
     record MyIp(String origin) {
     }
+
+    @PostExchange("/post")
+    Mono<String> upload(@RequestPart("purpose") String purpose, @RequestPart("file") Resource file);
 
 }
 
--- src/test/java/org/mvnsearch/httpinterface/HttpBinClientTest.java
+++ src/test/java/org/mvnsearch/httpinterface/HttpBinClientTest.java
@@ -2,6 +2,8 @@ package org.mvnsearch.httpinterface;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -21,4 +23,11 @@ public class HttpBinClientTest {
         System.out.println(httpBinClient.myIp("dabc").block().origin());
     }
 
+    @Test
+    public void testPost() throws Exception {
+        MultipartFile multipartFile = new MockMultipartFile("demo.txt", "demo.txt", "text/plain", "Hello World!".getBytes());
+        final String result = httpBinClient.upload("batch", multipartFile.getResource()).block();
+        System.out.println(result);
+
+    }
 }
